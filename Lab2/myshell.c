496508ff71191fcb2405f059cef74729499b3555
Xerocrassa <darren.chan1@uoit.net>
2019-01-29 16:25:32 -0500

    basic tokenizer working, string parsing from arg is not

--- Lab2/myshell.c
+++ Lab2/myshell.c
@@ -27,14 +27,24 @@ int main(int argc, char *argv[])
     char buffer[BUFFER_LEN] = {0};
     char command[BUFFER_LEN] = {0};
     char arg[BUFFER_LEN] = {0};
+    const char s[2] = " ";
 
     // Parse the commands provided using argc and argv
+    strcpy(buffer, argv[1]); //figure out how to copy the entire argument in buffer
+    printf ("%s", buffer);
 
     // Perform an infinite loop getting command input from users
     while (fgets(buffer, BUFFER_LEN, stdin) != NULL)
     {
         // Perform string tokenization to get the command and argument
-
+        char *token = strtok(buffer, s);
+        while (token) {
+            printf("token: %s\n", token);
+            //strcpy (token, command); cant copy token into command
+            printf("command is: %s\n", command);
+            token = strtok(NULL, s);
+        }
+        
         // Check the command and execute the operations for each command
         // cd command -- change the current directory
         if (strcmp(command, "cd") == 0)
